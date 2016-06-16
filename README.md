e090c306cae59af5ae77afa17e8f9b2f000949a4
oif <neowiz@icloud.com>
2016-06-16 17:00:53 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# IPFinder
+
+Version: Alpha
+
+
--- index.php
+++ index.php
@@ -28,7 +28,6 @@ class IPFinder extends SQLBase {
     }
 
     function getGeo() { // 获取物理地址
-
         return array('ip' => $this->ip, 'geo' => $this->queryIP($this->getStorageTable(), ip2long($this->ip)));
     }
 }
@@ -49,7 +48,6 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");    // API
 
 $apiResponse = array('err' => 'Invalid input', 'errCode' => '666');
-
 
 if (!empty($_SERVER['REQUEST_URI'])){
     $url = $_SERVER['REQUEST_URI'];
